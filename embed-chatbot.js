c10a33fbc2f93154a1e3849b61ffb38ad169e2ae
Viorica <95189482+vioricafoca@users.noreply.github.com>
2024-05-25 23:26:52 +0300

    Update embed-chatbot.js

--- embed-chatbot.js
+++ embed-chatbot.js
@@ -1,7 +1,7 @@
 (function() {
   async function loadChatbot() {
     try {
-      const response = await fetch('https://github.com/vioricafoca/chat-bot/blob/a93d2f6ec8e859b080ae4349c22e713913eb2b25/chat-bot');
+      const response = await fetch('https://raw.githubusercontent.com/vioricafoca/chat-bot/main/chat-bot.html');
       const content = await response.text();
       const container = document.createElement('div');
       container.innerHTML = content;
